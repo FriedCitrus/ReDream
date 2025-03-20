6a326c8aaf1b18360a57b60d52aecefa56e1776d
Reapser <22102004artem@gmail.com>
2025-03-20 18:20:54 +0200

    2

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static PlayerInput playerInput;
+
+    public static Vector2 moveInput;
+    public static bool JumpInputPressed;
+    public static bool JumpInputHeld;
+    public static bool JumpInputReleased;
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+    }
+
+    private void Update()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+        if (moveInput.x > 0)
+        {
+            Debug.Log("Right");
+        }
+        else if (moveInput.x < 0)
+        {
+            Debug.Log("Left");
+        }
+        JumpInputPressed = jumpAction.WasPressedThisFrame();
+        JumpInputHeld = jumpAction.IsPressed();
+        JumpInputReleased = jumpAction.WasReleasedThisFrame();
+    }
+}
